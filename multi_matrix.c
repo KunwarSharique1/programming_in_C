549b165fdcf7d40d67ba6d940389dfc808390b85
KunwarSharique1 <kunwar.sharique.cse.2025@miet.ac.in>
2026-07-31 12:20:20 +0530

    Add files via upload

---
+++ multi_matrix.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main(){
+    int mat1[3][3], mat2[3][3],
+ mat3[3][3];   
+    // int r1,r2,c1,c2;
+    
+//    printf("Enter the number of rows and cols for matrix 1: ");
+//    scanf("%d%d",&r1,&c1);
+    
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            scanf("%d",&mat1[i][j]);
+        }
+    }
+//     printf("Enter the number of rows and cols for matrix 2: ");
+//    scanf("%d%d",&r2,&c2);
+
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            scanf("%d",&mat2[i][j]);
+        }
+    }
+    
+  
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            mat3[i][j]=0;
+            for(int k=0;k<3;k++){
+            mat3[i][j]+=mat1[i][k]*mat2[k][j];
+            }
+        }
+    }
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+           printf("%d ",mat3[i][j]);
+        }
+        printf("\n");
+    }
+
+}
