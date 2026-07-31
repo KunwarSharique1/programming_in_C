2bec920c11c4c05217c427121f895874baf9e4b4
KunwarSharique1 <kunwar.sharique.cse.2025@miet.ac.in>
2026-07-31 11:42:21 +0530

    Add files via upload

---
+++ add_matrix.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main(){
+    int mat1[2][3];
+    int mat2[2][3];
+    int mat3[2][3];
+   
+    for(int i=0;i<2;i++){
+        for(int j=0;j<3;j++){
+            scanf("%d",&mat1[i][j]);
+        }
+    }
+    for(int i=0;i<2;i++){
+        for(int j=0;j<3;j++){
+            scanf("%d",&mat2[i][j]);
+        }
+    }
+    for(int i=0;i<2;i++){
+        for(int j=0;j<3;j++){
+            mat3[i][j]=mat1[i][j]+mat2[i][j];
+        }
+    }
+    for(int i=0;i<2;i++){
+        for(int j=0;j<3;j++){
+           printf("%d ",mat3[i][j]);
+        }
+        printf("\n");
+    }
+
+}
